6e1286f0e2f99773a39a8ba7a1006dc5b1db9221
SwarajKhadse <khadseswaraj@gmail.com>
2022-10-09 02:47:44 +0530

    port updated2

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ io.on('connection',(socket)=>
 // console.log(`server started on port ${PORT}`)
 // });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
